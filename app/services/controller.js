27ff2d162fa4e7c1ddfa37be90d8126aa594085c
CristianUser <cristianmejia97@gmail.com>
2020-11-25 08:25:55 -0400

    add search capabilities

--- app/services/controller.js
+++ app/services/controller.js
@@ -1,38 +1,79 @@
+const { Op } = require('sequelize');
+const { parseJson } = require('./utils');
+const { getModelHelper } = require('./db');
+const log = require('./log')({ file: __filename });
+
+/**
+ * @param {import('sequelize').ModelCtor<import('sequelize').Model>} model Sequelize model
+ */
 function createController(model) {
+  const controller = getModelHelper(model);
+
   return {
-    get({ query: { rows = 10, page = 1 } }) {
-      return model.findAndCountAll({
-        limit: rows,
-        offset: (page - 1) * rows
+    async get({ ctx, query: { rows = 10, page = 1, match, search, sortBy } }) {
+      const pagination =
+        rows != -1
+          ? {
+              limit: rows,
+              offset: (page - 1) * rows
+            }
+          : {};
+      const where = parseJson(match);
+      const searchEntries = Object.entries(parseJson(search) || {});
+      const like = searchEntries.length
+        ? {
+            [Op.or]: searchEntries.map(([key, value]) => {
+              return {
+                [key]: {
+                  [Op.iLike]: `%${value}%`
+                }
+              };
+            })
+          }
+        : {};
+      const order = {
+        order: Object.entries(parseJson(sortBy) || {})
+      };
+
+      const results = await model.findAndCountAll({
+        ...pagination,
+        ...order,
+        where: { ...where, ...like, ...ctx.where }
       });
+
+      if (ctx.parser) await ctx.parser(results);
+
+      return results;
     },
-    getById({ params: { id } }) {
-      return model.findAll({ where: { id } }).then(rows => rows[0]);
+    async getById({ ctx, params: { id } }) {
+      const result = await controller.get(id);
+
+      if (ctx.parser) await ctx.parser(result);
+      return result;
     },
     post({ body }) {
-      return model.create(body);
+      return controller.post(body);
     },
-    put({ params: { id }, body }) {
-      return model.update(body, {
-        where: {
-          id
-        }
-      });
+    put({ params: { id }, query: { upsert }, body }) {
+      return controller.put(id, body, upsert);
+    },
+    patch({ params: { id }, body }) {
+      return controller.patch(id, body);
     },
     delete({ params: { id } }) {
-      return model.destroy({ where: { id } });
+      return controller.delete(id);
     }
   };
 }
 
-module.exports = createController;
-
+module.exports.createController = createController;
 module.exports.generateDefaultRoutes = (router, model) => {
   const routes = [
     { method: 'get' },
     { method: 'post' },
     { method: 'get', param: 'id', func: 'getById' },
     { method: 'put', param: 'id' },
+    { method: 'patch', param: 'id' },
     { method: 'delete', param: 'id' }
   ];
 
@@ -40,12 +81,13 @@ module.exports.generateDefaultRoutes = (router, model) => {
 
   routes.forEach(route => {
     router[route.method](route.param ? '/:id' : '/', (req, res) => {
-      controller[route.func || route.method](req)
+      return controller[route.func || route.method](req)
         .then(data => {
           res.json(data);
         })
         .catch(err => {
-          res.status(400).send(err.message);
+          res.status(err.status || 400).send({ error: err.message });
+          log.error(err.message, { stack: err.stack });
         });
     });
   });
