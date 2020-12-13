3e273fd1a5e7ffdee6b5f4d2c0b24055c9010a03
Cristian Mejia <cristianmejia97@gmail.com>
2020-12-12 23:06:51 -0400

    ARS-16: add default definitions

--- app/config/swagger/index.js
+++ app/config/swagger/index.js
@@ -9,22 +9,53 @@ const swaggerDocument = parseYaml(`${__dirname}/openapi.yml`);
 _.set(swaggerDocument, 'info.description', pkg.description);
 _.set(swaggerDocument, 'info.version', pkg.version);
 
-module.exports = app => {
+function loadFiles(models) {
   getFiles('app/entities/**/swagger.*').forEach(file => {
     const swaggerFile = parseYaml(file);
     const entity = getEntityName(file);
-    const namespace = `/${entity}`;
+    const route = `/${entity}`;
+    const model = models[entity];
+    const modelName = [model.name, model.getTableName()];
 
     Object.keys(swaggerFile.paths).forEach(key => {
       const path = swaggerFile.paths[key];
+      const namespace = `${route}${key}`.replace(/\/$/, '');
 
+      path.tags = [modelName[1]];
       delete swaggerFile.paths[key];
 
-      swaggerFile.paths[`${namespace}${key}`] = path;
+      swaggerFile.paths[namespace] = path;
     });
 
+    console.log(swaggerFile);
     _.merge(swaggerDocument, swaggerFile);
   });
+}
+
+function defaultDefinition(modelName) {
+  const referenceFile = JSON.stringify(parseYaml(`${__dirname}/default-definitions.yml`));
+
+  return JSON.parse(
+    referenceFile
+      .replace(/<Plural>/g, modelName[1])
+      .replace(/<plural>/g, modelName[1].toLocaleLowerCase())
+      .replace(/<Singular>/g, modelName[0])
+      .replace(/<singular>/g, modelName[0].toLocaleLowerCase())
+  );
+}
+
+function addDefaultDefinitions(models) {
+  Object.keys(models).forEach(entity => {
+    const model = models[entity];
+    const modelName = [model.name, model.getTableName()];
+
+    _.merge(swaggerDocument, defaultDefinition(modelName));
+  });
+}
+
+module.exports = (app, models) => {
+  addDefaultDefinitions(models);
+  loadFiles(models);
 
   app.use('/_docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 };
